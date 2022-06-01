81032c49a9f5aeceae485a372999f6a975b87ad5
kompotkot <zenitsws@gmail.com>
2022-06-01 18:26:12 +0000

    Backend moved to orb

--- backend/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# Expects access to Python environment with the requirements for this project installed.
-set -e
-
-CEM_BACKEND_HOST="${CEM_BACKEND_HOST:-0.0.0.0}"
-CEM_BACKEND_PORT="${CEM_BACKEND_PORT:-9898}"
-
-uvicorn --port "$CEM_BACKEND_PORT" --host "$CEM_BACKEND_HOST" backend.api:app --reload
